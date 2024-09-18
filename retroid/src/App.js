49cb38986b98c0b0451b9a096dad2efef9e47a53
Mahylan <mahylan.veclin@wangarden.com>
2024-09-18 10:06:06 +0200

    navbar component

--- retroid/src/App.js
+++ retroid/src/App.js
@@ -1,163 +1,40 @@
 import './App.css';
-
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Panier from './screens/panier';
 import ProductList from './screens/product_list';
 import ProductDetails from './screens/product_details';
 import Fondsecrans from './screens/fonds-ecrans';
 import Carousel1 from './screens/caroussel';
-
-const navigation = [
-  { name: "L'ATELIER", href: '/', current: false },
-  { name: 'PSVITA – OLED', href: '/panier', current: false },
-  { name: 'ÉDITIONS LIMITÉES', href: '/product-list', current: false },
-  { name: 'ACCESSOIRES', href: '/product-details', current: false },
-  { name: 'FONDS D’ÉCRAN', href: '/fonds-ecrans', current: false },  
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
+import Navbar from './Navbar';  // Importer le composant Navbar
 
 export default function App() {
   return (
     <Router>
-      <Disclosure as="nav" className="bg-slate-50">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              {/* Mobile menu button */}
-              <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                <span className="sr-only">Open main menu</span>
-                <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                <XMarkIcon className="hidden h-6 w-6" aria-hidden="true" />
-              </DisclosureButton>
-            </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex flex-shrink-0 items-center">
-                <img
-                  className="h-8 w-auto"
-                  src="/logo.webp"
-                  alt="Metro retroid"
-                />
-              </div>
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      to={item.href}
-                      className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-700 hover:text-white',
-                        'rounded-md px-3 py-2 text-sm font-medium',
-                      )}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <button
-                type="button"
-                className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              >
-                <span className="sr-only">View notifications</span>
-                <BellIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-
-              {/* Profile dropdown */}
-              <Menu as="div" className="relative ml-3">
-                <div>
-                  <MenuButton className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                    <span className="sr-only">Open user menu</span>
-                    <img
-                      className="h-8 w-8 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </MenuButton>
-                </div>
-                <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <MenuItem>
-                    {({ active }) => (
-                      <Link
-                        to="/profile"
-                        className={classNames(
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700'
-                        )}
-                      >
-                        Your Profile
-                      </Link>
-                    )}
-                  </MenuItem>
-                  <MenuItem>
-                    {({ active }) => (
-                      <Link
-                        to="/settings"
-                        className={classNames(
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700'
-                        )}
-                      >
-                        Settings
-                      </Link>
-                    )}
-                  </MenuItem>
-                  <MenuItem>
-                    {({ active }) => (
-                      <Link
-                        to="/logout"
-                        className={classNames(
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700'
-                        )}
-                      >
-                        Sign out
-                      </Link>
-                    )}
-                  </MenuItem>
-                </MenuItems>
-              </Menu>
-            </div>
-          </div>
-        </div>
-
-        <DisclosurePanel className="sm:hidden">
-          <div className="space-y-1 px-2 pb-3 pt-2">
-            {navigation.map((item) => (
-              <DisclosureButton
-                key={item.name}
-                as={Link}
-                to={item.href}
-                className={classNames(
-                  item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'block rounded-md px-3 py-2 text-base font-medium'
-                )}
-              >
-                {item.name}
-              </DisclosureButton>
-            ))}
-          </div>
-        </DisclosurePanel>
-      </Disclosure>
-      <section>
-        <Carousel1 />
-      </section>
+      <Navbar />  {/* Navigation */}
+      
       <main>
         <Routes>
-          <Route path="/" element={<h1>Welcome to the Homepage</h1>} />
+          {/* Carousel uniquement sur la page d'accueil */}
+          <Route path="/" element={
+            <>
+              <Carousel1 />
+              <h1>Welcome to the Homepage</h1>
+            </>
+          } />
+          
+          {/* Route pour le panier */}
           <Route path="/panier" element={<Panier />} />
+
+          {/* Route pour la liste des produits */}
           <Route path="/product-list" element={<ProductList />} />
+
+          {/* Route pour les détails du produit */}
           <Route path="/product-details" element={<ProductDetails />} />
+
+          {/* Route pour les fonds d'écran */}
           <Route path="/fonds-ecrans" element={<Fondsecrans />} />
         </Routes>
       </main>
     </Router>
   );
-
 }
